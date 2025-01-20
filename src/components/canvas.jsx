18c6aa6911fcc2a8007b06949aeacd476fe142d9
Baptiste Fkt <18186452+baptistefkt@users.noreply.github.com>
2025-01-20 19:38:33 +0100

    fix camera movement

--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -1,59 +1,63 @@
-import { useLayoutEffect, useEffect, useRef, useState } from 'react'
+import { useLayoutEffect, useEffect, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { Lightformer, useMask, useGLTF, useAnimations, Float, CameraControls, Environment, RandomizedLight, AccumulativeShadows, MeshTransmissionMaterial, OrbitControls } from '@react-three/drei'
-import { easing } from 'maath'
+import { useMask, useGLTF, useAnimations, Float, CameraControls, Environment, RandomizedLight, AccumulativeShadows, MeshTransmissionMaterial } from '@react-three/drei'
 
 
-export default function MyCanvas() {  
+export default function MyCanvas() {
   return (
     <Canvas shadows camera={{ position: [30, 0, -3], fov: 35, near: 1, far: 50 }}>
-      {/* <color attach="background" args={['#c6dde5']} /> */}
+
       <Aquarium position={[0, 0.025, 0]} >
         <Float rotationIntensity={1} floatIntensity={1} speed={2}>
           <Tuna position={[0, 0.4, 0]} rotation={[0, Math.PI, 0]} scale={3} />
         </Float>
       </Aquarium>
-      {/** Soft shadows */}
+
       <AccumulativeShadows temporal frames={100} color="#aaaaaa" colorBlend={2} opacity={0.7} scale={60} position={[0, -5, 0]}>
         <RandomizedLight amount={8} radius={15} ambient={0.5} intensity={1} position={[-5, 10, -5]} size={20} />
       </AccumulativeShadows>
-      {/** Custom environment map */}
-      {/* <Environment resolution={1024}>
-        <group rotation={[-Math.PI / 3, 0, 0]}>
-          <Lightformer intensity={4} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
-          {[2, 0, 2, 0, 2, 0, 2, 0].map((x, i) => (
-            <Lightformer key={i} form="circle" intensity={4} rotation={[Math.PI / 2, 0, 0]} position={[x, 4, i * 4]} scale={[4, 1, 1]} />
-          ))}
-          <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[50, 2, 1]} />
-          <Lightformer intensity={2} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[50, 2, 1]} />
-        </group>
-      </Environment> */}
-      <Environment background preset="apartment" blur={0.8} />
+
+      <Environment resolution={1024} background preset="apartment" blur={0.8} />
+
       <CameraController />
     </Canvas>
   )
 }
 
-const degToRad = (degrees) => degrees * (Math.PI / 180);
+const mapRange = (value, inputStart, inputEnd, outputStart, outputEnd) => {
+  return outputStart + (outputEnd - outputStart) * ((value - inputStart) / (inputEnd - inputStart));
+};
+
+const mapPointerY = (pointerY) => {
+  if (pointerY <= 0) {
+    return 90;
+  } else {
+    return 90 + (0 - 90) * pointerY;
+  }
+};
+
 
 function CameraController() {
   const cameraRef = useRef();
-    useFrame((state, delta) => {
-      cameraRef.current.rotate(Math.sin(state.pointer.x/15), -Math.sin(state.pointer.y / 15), true);
+
+  useFrame((state) => {
+    const angleX = mapRange(state.pointer.x, -1, 1, 0, 180);
+    const angleXInRadians = (angleX * Math.PI) / 180;
+
+    const angleYInDegrees = mapPointerY(state.pointer.y);
+    const angleYInRadians = (angleYInDegrees * Math.PI) / 180;
+
+    cameraRef.current.rotateTo(angleXInRadians, angleYInRadians, true);
   })
+
+  useEffect(() => {
+    cameraRef?.current?.disconnect();
+  }, [])
+
   return (
-    <CameraControls ref={cameraRef} truckSpeed={0} dollySpeed={0} minPolarAngle={degToRad(30)} maxPolarAngle={degToRad(90)} maxAzimuthAngle={degToRad(160)} minAzimuthAngle={degToRad(20)} />
+    <CameraControls ref={cameraRef} truckSpeed={0} dollySpeed={0} />
   )
 }
-
-// function CameraRig() {
-//   useFrame((state, delta) => {
-//     // easing.damp3(state.camera.position, [-state.pointer.x * 10, state.pointer.y*5 +4, 35], 0.3, delta)
-//     // easing.damp3(ref.current.rotation, [Math.PI / 2, 0, state.clock.elapsedTime / 5 + state.pointer.x], 0.2, delta)
-//       easing.damp3(state.camera.position, [Math.cos(state.pointer.x / 4) * 35, 4 + state.pointer.y * 8, Math.sin(state.pointer.x / 4) * 35], 0.5, delta)      
-//     state.camera.lookAt(0, 0, 0)
-//   })
-// }
 
 function Aquarium({ children, ...props }) {
   const ref = useRef()
@@ -78,7 +82,7 @@ function Aquarium({ children, ...props }) {
           iridescence={1}
           iridescenceIOR={1}
           iridescenceThicknessRange={[0, 1400]}
-          // color={"#e5eb98"}
+        // color={"#e5eb98"}
         />
       </mesh>
       <group ref={ref}>{children}</group>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      {/* <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> */}
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       </Head>
       <body className="antialiased">
         <Main />
